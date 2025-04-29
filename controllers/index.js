f514b13214d76b89bf8d1c895db6103cb61ef4d3
VINIT286 <vinitgandhe10301@gmail.com>
2025-04-29 12:43:44 +0530

    add files

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,25 @@
+// var express = require('express')
+// var app = express()
+// //------------routes-----------
+// var routes = require("./routes/routes.js")
+// app.use(routes)
+// //===========================
+// app.listen(3000,()=>{
+//     console.log("running")
+// })
+
 var express = require('express')
-var app = express()
-//------------routes-----------
-var routes = require("./routes/routes.js")
-app.use(routes)
-//===========================
-app.listen(3000,()=>{
-    console.log("running")
+var app = express();
+// const dotenv = require('dotenv')
+// dotenv.config();
+// port = process.env.PORT
+
+const port = require('./config/envConfig.js')
+
+// const server=http.createServer((req,res)=>{
+//     res.write("hello")
+//     res.end()
+// })
+app.listen(port,()=>{
+    console.log(`runnin on ${port} port`)
 })
